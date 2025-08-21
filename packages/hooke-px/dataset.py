29e17a286d19adad0842eb1494a4d936105ecbd3
C Jones <charlie@valencelabs.com>
2025-08-21 07:27:03 -0600

    don't do multiscale by default

--- packages/hooke-px/dataset.py
+++ packages/hooke-px/dataset.py
@@ -219,7 +219,7 @@ def get_dataloaders(
     batch_size: int = ornamentalist.Configurable[64],
     num_workers: int = ornamentalist.Configurable[16],
     pin_memory: bool = ornamentalist.Configurable[True],
-    multiscale: bool = ornamentalist.Configurable[True],
+    multiscale: bool = ornamentalist.Configurable[False],
 ) -> tuple[DataLoader, DataLoader]:
     df = pl.read_parquet(path)
     train_df = df.filter(pl.col("split") == "train")
